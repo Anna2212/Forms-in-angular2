2dd0f3a5a39f3c8b95e2b8102790d942617fc494
Anna <medvidanja94@gmail.com>
2017-11-13 00:18:59 +0200

    changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, FormArray, FormArrayName } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, FormArray, FormGroupName } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -10,37 +10,67 @@ export class HomeComponent implements OnInit {
 
   public infoForm: FormGroup;
   public visitedСountriesControls: FormArray;
+  public maritalS = ['married', 'unmarried']
 
   constructor(private formBuilder: FormBuilder) { 
     this.buildForm();
   }
 
   buildForm (){
-    this.infoForm = this.formBuilder.group({
-      firstName: this.formBuilder.control(null),
-      lastName: this.formBuilder.control(null),
-      phoneNumber: this.formBuilder.control(null),
-      bio: this.formBuilder.control(null),
-      maritalStatus: this.formBuilder.control(null),
+    this.infoForm = new FormGroup({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      phoneNumber: new FormControl(''),
+      bio: new FormControl(''),
+      email: new FormControl(''),
+      maritalStatus: new FormControl('unmarried'),
       visitedСountries: this.formBuilder.array([
-        this.formBuilder.control(null)
+        this.initVisitedСountries()
       ]),
+      musicStyle: new FormGroup({
+        rok: new FormControl(false),
+        pop: new FormControl(false),
+        jazz: new FormControl(''),
+        funk: new FormControl(''),     
+        rap: new FormControl('')
+      }),
+      color: new FormControl('#ffffff')
     });
     this.visitedСountriesControls = this.infoForm.get('visitedСountries') as FormArray;
+    console.dir(this.infoForm)
   }
-  public onAddVisitedCountries (){
-    this.visitedСountriesControls.push(this.formBuilder.control(null));
-  }
-
-  public onRemoveVisitedCountries (index){
-    this.visitedСountriesControls.removeAt(index);
-  }
+ 
   public onResetForm (){
     this.infoForm.reset();
   }
 
   public onSubmitForm (){
     console.log(this.infoForm.value);
+  }
+
+  private initVisitedСountries() {
+    return this.formBuilder.group({
+      name: new FormControl(null)
+    });
+  }
+
+  private initVisitedCountriesAtCreate() {
+    const control = <FormArray>this.infoForm.controls['visitedСountries'];
+    const initData = [{
+      name: null
+    }];
+    initData.forEach(data => control.push(this.formBuilder.group(data)));
+  }
+
+  public onAddVisitedCountries() {
+    const control = <FormArray>this.infoForm.controls['visitedСountries'];
+    const addCtrl = this.initVisitedСountries();
+    control.push(addCtrl);
+  }
+
+  public onRemoveVisitedCountries(i: number) {
+    const control = <FormArray>this.infoForm.controls['visitedСountries'];
+    control.removeAt(i);
   }
   ngOnInit() {
   }
